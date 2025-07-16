35542c8c46e451c8f248523330b3ae0402a62966
Pushkar Gokhale <pushkar.gokhale@globant.com>
2025-07-16 11:33:45 +0530

    Added Husky, Code Formatting & StyleCop reviews

--- obj/Debug/net8.0/PRSummerization.AssemblyInfo.cs
+++ obj/Debug/net8.0/PRSummerization.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRSummerization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a99cbc4817e4c0efae187a06986e6aef4d70658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf03725098169ab2f61363f7f60b1701c23f1ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRSummerization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRSummerization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
